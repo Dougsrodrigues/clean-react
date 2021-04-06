a89247fa79c71cfcf5c764219ee51c6252c6baa5
Douglas Santiago <dougsrodrigues@outlook.com>
2021-04-06 20:51:12 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './scr/main/index.tsx',
+  entry: './src/main/index.tsx',
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
