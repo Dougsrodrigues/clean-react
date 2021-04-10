aa1759b199a238854169b37a9c3af125972f63ee
Douglas Santiago <dougsrodrigues@outlook.com>
2021-04-10 17:16:10 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -26,7 +26,7 @@ const Login: FC = () => {
 
           <Input type="password" name="password" placeholder="Digite sua senha" />
 
-          <button className={Styles.submit} type="submit">Entrar</button>
+          <button className={Styles.submit} disabled data-testid="submit" type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
         </form>
