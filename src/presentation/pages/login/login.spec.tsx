9abd2792f3b91eec2028a1f4f0e149f9ac895e4a
Douglas <dougsrodrigues@outlook.com>
2021-10-08 15:43:17 -0300

    test: ensure login present valid status if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -73,4 +73,22 @@ describe("Login Component", () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe("🔴");
   });
+
+  test("Should show valid password state if Validation succeeds", () => {
+    const { sut, validationStub } = makeSut();
+
+    validationStub.errorMessage = null;
+
+    const password = faker.internet.password();
+
+    const passwordInput = sut.getByTestId("password");
+
+    fireEvent.input(passwordInput, { target: { value: password } });
+
+    const passwordStatus = sut.getByTestId("password-status");
+
+    expect(passwordStatus.title).toBe("Tudo certo!");
+
+    expect(passwordStatus.textContent).toBe("🟢");
+  });
 });
