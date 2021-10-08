139b816a5089713f24d9855bcde8099a3bce371d
Douglas <dougsrodrigues@outlook.com>
2021-10-08 15:43:08 -0300

    feat: ensure login present valid status if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,34 +1,46 @@
-import React, { useContext } from 'react';
-import Styles from './input-styles.scss';
-import Context from '@/presentation/contexts/form/form-context'
+import React, { useContext } from "react";
+import Styles from "./input-styles.scss";
+import Context from "@/presentation/contexts/form/form-context";
 
-type Props = React.InputHTMLAttributes<HTMLInputElement>
+type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Footer: React.FC<Props> = (props: Props) => {
-  const { state, setState } = useContext(Context)
-  const error = state[`${props.name}Error`]
+  const { state, setState } = useContext(Context);
+  const error = state[`${props.name}Error`];
   function enableInput(event: React.FocusEvent<HTMLInputElement>): void {
-    event.target.readOnly = false
+    event.target.readOnly = false;
   }
 
   function handleChange(event: React.FocusEvent<HTMLInputElement>): void {
-    setState({ ...state, [event.target.name]: event.target.value })
+    setState({ ...state, [event.target.name]: event.target.value });
   }
 
   const getStatus = (): string => {
-    return '🔴'
-  }
+    return error ? "🔴" : "🟢";
+  };
 
   const getTitle = (): string => {
-    return error
-  }
+    return error || "Tudo certo!";
+  };
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
-      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+      <input
+        {...props}
+        data-testid={props.name}
+        readOnly
+        onFocus={enableInput}
+        onChange={handleChange}
+      />
+      <span
+        data-testid={`${props.name}-status`}
+        title={getTitle()}
+        className={Styles.status}
+      >
+        {getStatus()}
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
