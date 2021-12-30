9929a7ce6b003f0cffd4b26d39a8dfb3f68298e6
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-30 15:06:06 -0300

    feat: prevent login to call submit more then once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -35,6 +35,7 @@ const Login: FC<Props> = ({ validation, authentication }) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (state.isLoading) return
     setState({ ...state, isLoading: true });
     await authentication.auth({ email: state.email, password: state.password });
   };
