a59cdbba7e28bed8fb26e291936ef2698be7f80e
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 10:45:32 -0300

    feat: ensure RequiredFieldValidation returns error if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from "../errors"
 import { RequiredFieldValidation } from "./required-field-validation"
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
@@ -11,5 +12,15 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError(field))
+  })
+
+  test('Should return false if field is not empty', () => {
+    const field = 'email'
+
+    const sut = new RequiredFieldValidation(field)
+
+    const error = sut.validate(faker.random.word())
+
+    expect(error).toBeFalsy()
   })
 })
