8ea8dc8a7d6e4e9407883d82c0dcec3954cb93db
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 15:13:10 -0300

    feat: ensure validaiton composite returns error if any validation fails

---
+++ src/validation/validation-composite/validation-composite.ts
@@ -0,0 +1,15 @@
+import { Validation } from "@/presentation/protocols/validations"
+import { FieldValidation } from "../protocols/field-validation"
+
+export class ValidationComposite implements Validation {
+  constructor(private readonly validators: FieldValidation[]) { }
+
+  validate(fieldName: string, fieldValue: string): string {
+    const filterValidators = this.validators.filter(validator => validator.field === fieldName)
+
+    for (const validator of filterValidators) {
+      const error = validator.validate(fieldValue)
+      if (error) return error.message
+    }
+  }
+}
