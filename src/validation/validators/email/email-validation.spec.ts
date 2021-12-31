5fab97aed8ada3de316c5fb2dee0c546157dc3c0
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 11:20:59 -0300

    test: ensure EmailValidation returns false  if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,5 +1,6 @@
 import { InvalidFieldError } from "@/validation/errors/invalid-field-error";
 import { EmailValidation } from "./email-validation";
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if emails is invalid', () => {
@@ -7,8 +8,18 @@ describe('EmailValidation', () => {
 
     const sut = new EmailValidation(field)
 
-    const error = sut.validate(field)
+    const error = sut.validate(faker.random.word())
 
     expect(error).toEqual(new InvalidFieldError(field))
+  })
+
+  test('Should return false if emails is valid', () => {
+    const field = 'email';
+
+    const sut = new EmailValidation(field)
+
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
