3c2e9450e75e7efd493ef19f7b51a229b23ab613
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 12:27:13 -0300

    test: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidFieldError } from "@/validation/errors/invalid-field-error"
+import { MinLengthValidation } from "./min-length-validation"
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const field = 'any_field'
+
+    const sut = new MinLengthValidation(field, 5)
+
+    const error = sut.validate('123')
+
+    expect(error).toEqual(new InvalidFieldError(field))
+  })
+})
