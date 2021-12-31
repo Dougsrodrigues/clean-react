b889b1e10f884fdcba41ed61691aa56a858fa880
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 15:34:01 -0300

    test: ensure validation composite returns false if there is no error

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 
 import { FieldValidationSpy } from "../validators/test/mock-field-validation"
 import { ValidationComposite } from "./validation-composite"
+import faker from 'faker'
 
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
-  const fieldValidationSpy = new FieldValidationSpy('any_field')
-  const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+const makeSut = (fieldName: string): SutTypes => {
+  const fieldValidationSpy = new FieldValidationSpy(fieldName)
+  const fieldValidationSpy2 = new FieldValidationSpy(fieldName)
 
   const fieldValidationsSpy = [fieldValidationSpy, fieldValidationSpy2]
 
@@ -20,14 +21,24 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
 
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
 
-    fieldValidationsSpy[0].error = new Error('first_any_error')
-    fieldValidationsSpy[1].error = new Error('second_any_error')
+    const error = sut.validate(fieldName, faker.random.words())
 
-    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe(errorMessage)
+  })
 
-    expect(error).toBe('first_any_error')
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+
+    const error = sut.validate(fieldName, faker.random.words())
+
+    expect(error).toBeFalsy()
   })
 })
