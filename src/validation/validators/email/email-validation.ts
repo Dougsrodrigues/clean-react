e018e344d6d5b7c7e0d54a9d3b26413cedc44adc
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 14:56:10 -0300

    feat: ensure email validation allows empty values

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -7,7 +7,7 @@ class EmailValidation implements FieldValidation {
   validate(value: string): InvalidFieldError {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-    return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError(this.field)
   }
 }
 
