11332cd5de51089c67d74347d011df76071e5be0
Douglas Santiago <dougsrodrigues@outlook.com>
2021-12-31 18:06:54 -0300

    fix: ensure FieldValidation will be import of correct place

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -1,5 +1,5 @@
 import { Validation } from "@/presentation/protocols/validations"
-import { FieldValidation } from "../protocols/field-validation"
+import { FieldValidation } from "@/validation/protocols/field-validation"
 
 export class ValidationComposite implements Validation {
   constructor(private readonly validators: FieldValidation[]) { }
