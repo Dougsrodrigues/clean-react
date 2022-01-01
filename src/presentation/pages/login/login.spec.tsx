d1635d5164b3874dcf6bc98cb3203114ab997c31
Douglas Santiago <dougsrodrigues@outlook.com>
2022-01-01 14:45:07 -0300

    refactor: change import of login component

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,7 +9,7 @@ import {
   waitFor,
 } from "@testing-library/react";
 import 'jest-localstorage-mock'
-import Login from "./login";
+import { Login } from "@/presentation/pages";
 import { AuthenticationSpy, ValidationStub } from "@/presentation/test";
 import faker from "faker";
 import { InvalidCredentialsError } from "@/domain/errors";
