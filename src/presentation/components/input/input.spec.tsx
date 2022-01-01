7c71280181f0c54703229646db7ffa4480b8b3ad
Douglas Santiago <dougsrodrigues@outlook.com>
2022-01-01 14:56:06 -0300

    test: ensure input remove readOnly on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import Input from './input'
 import Context from "@/presentation/contexts/form/form-context";
 
@@ -18,5 +18,14 @@ describe('Input Component', () => {
     const input = sut.getByTestId('field') as HTMLInputElement
 
     expect(input.readOnly).toBeTruthy()
+  })
+
+  test('Should remove readOnly on focus', () => {
+    const sut = makeSut()
+
+    const input = sut.getByTestId('field') as HTMLInputElement
+    fireEvent.focus(input)
+
+    expect(input.readOnly).toBeFalsy()
   })
 })
