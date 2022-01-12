82e4911eb3726d0471879ff8f54078e852e56272
Douglas Santiago <dougsrodrigues@outlook.com>
2022-01-12 19:47:38 -0300

    test: ensure SignUp calls addaccount only once

--- src/presentation/test/mock-add-account.ts
+++ src/presentation/test/mock-add-account.ts
@@ -5,11 +5,13 @@ import { AddAccount, AddAccountParams } from '@/domain/usecases';
 export class AddAccountSpy implements AddAccount {
   account = mockAccountModel();
 
+  callsCount = 0;
+
   params: AddAccountParams;
 
   async add(params: AddAccountParams): Promise<AccountModel> {
     this.params = params;
-
+    this.callsCount++;
     return this.account;
   }
 }
